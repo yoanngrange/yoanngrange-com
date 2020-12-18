36ff820f28b00d9d2f68776c927009c8994f078c
Yoann Grange <yoann.grange@gmail.com>
2020-12-18 14:53:29 +0100

    Update head.js

--- js/head.js
+++ js/head.js
@@ -12,6 +12,15 @@ class Head extends HTMLElement {
     <link rel="preconnect" href="https://fonts.gstatic.com" />
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap" rel="stylesheet" />
     <link href="css/styles.css" rel="stylesheet" />
+
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="mask-icon" href="safari-pinned-tab.svg" color="#262173">
+    <meta name="msapplication-TileColor" content="#262173">
+    <meta name="theme-color" content="#262173">
+
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-2771720-9"></script>
     <script>
