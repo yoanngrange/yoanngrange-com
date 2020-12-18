843b9e680afbd621745c86788fc593784f3774cc
Yoann Grange <yoann.grange@gmail.com>
2020-12-18 14:34:53 +0100

    Update head.js

--- js/head.js
+++ js/head.js
@@ -4,7 +4,10 @@ lass Head extends HTMLElement {
   }
 
   connectedCallback() {
-    this.innerHTML = `    
+    this.innerHTML = `
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="author" content="Yoann Grange" />
     <link href="css/bootstrap.min.css" rel="stylesheet" />
     <link rel="preconnect" href="https://fonts.gstatic.com" />
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap" rel="stylesheet" />
