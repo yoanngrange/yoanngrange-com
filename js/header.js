1cfc5524ad1057e3964f97a0eed66bb53848728e
Yoann Grange <yoann.grange@gmail.com>
2020-12-18 14:58:55 +0100

    Update header.js

--- js/header.js
+++ js/header.js
@@ -7,9 +7,9 @@ class Header extends HTMLElement {
     this.innerHTML = `
     <section class="jumbotron text-center">
         <div class="container">
-            <h1 class="jumbotron-heading"><a href="index.html">Yoann Grange</a></h1>
-            <p class="lead">Product Manager, Marketer &amp; Designer</p>
-            <p><a href="https://twitter.com/yoanngrange" target="_blank">@yoanngrange</a> - <a href="mailto:yoann.grange@gmail.com">yoann.grange@gmail.com</a></p>
+            <h1><a href="index.html">Yoann Grange</a></h1>
+            <p class="p1">Product Manager, Marketer &amp; Designer</p>
+            <p class="p2"><a href="https://twitter.com/yoanngrange" target="_blank">@yoanngrange</a> - <a href="mailto:yoann.grange@gmail.com">yoann.grange@gmail.com</a></p>
         </div>
     </section>
     `;
