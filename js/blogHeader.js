57bf44a693b7d9fae88bd404f9c9ed1e9014cd0b
Yoann Grange <yoann.grange@gmail.com>
2023-05-10 10:49:25 +0200

    Update blogHeader.js

--- js/blogHeader.js
+++ js/blogHeader.js
@@ -8,8 +8,7 @@ class blogHeader extends HTMLElement {
     <section class="jumbotron text-center">
         <div class="container">
             <h2><a href="../index.html">Yoann Grange</a></h2>
-            <p class="p1">Product Manager, Marketer &amp; Designer</p>
-            <p class="p2"><a href="https://twitter.com/yoanngrange" target="_blank" rel="noreferrer">@yoanngrange</a> - <a href="mailto:yoann.grange@gmail.com">yoann.grange@gmail.com</a></p>
+            <p class="p2"><a href="mailto:yoann.grange@gmail.com">yoann.grange@gmail.com</a></p>
         </div>
     </section>
     `;
